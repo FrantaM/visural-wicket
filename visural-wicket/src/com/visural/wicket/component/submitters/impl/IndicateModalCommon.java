2b8628ce30ed4ae0d57ab2fb1f9830f4ab40bf95
František Mejta <frantisek.mejta@studentagency.cz>
2013-03-25 18:47:24 +0100

    - Aktualizace definic knihoven pro Netbeans. - Oprava přidávání refrence na css pro modální dialog. - Změna verze na snapshot.

--- visural-wicket/src/com/visural/wicket/component/submitters/impl/IndicateModalCommon.java
+++ visural-wicket/src/com/visural/wicket/component/submitters/impl/IndicateModalCommon.java
@@ -1,12 +1,12 @@
 /*
  *  Copyright 2010 Richard Nichols.
- * 
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- * 
+ *
  *       http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -39,7 +39,7 @@ import com.visural.wicket.util.PageParamFactory;
  */
 public class IndicateModalCommon implements Serializable {
     private static final long serialVersionUID = 1L;
-    
+
     private final IndicateModalComponent imComponent;
     private final Component component;
 
@@ -57,7 +57,7 @@ public class IndicateModalCommon implements Serializable {
             component.add(new Behavior() {
                 @Override
                 public void renderHead(Component component, IHeaderResponse response) {
-                    response.render(JavaScriptReferenceHeaderItem.forReference(new ModalCSSRef()));
+                    response.render(CssHeaderItem.forReference(new ModalCSSRef()));
                 }
             });
             if (com.isSupportIE6()) {
